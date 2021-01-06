6464fb713fa354be2b998c2437bb01170458405e
Alonso Essenwanger <alonsohe@hotmail.com>
2021-01-06 09:02:43 +0100

    Patient

---
+++ ChariteTest/Patient.swift
@@ -0,0 +1,8 @@
+//
+//  Patiendt.swift
+//  ChariteTest
+//
+//  Created by Alonso Essenwanger on 22.12.20.
+//
+
+import Foundation
---
+++ ChariteTest/RegisterLogin.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterLogin.swift
+//  ChariteTest
+//
+//  Created by Alonso Essenwanger on 24.12.20.
+//
+
+import SwiftUI
+
+struct RegisterLogin: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RegisterLogin_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterLogin()
+    }
+}
